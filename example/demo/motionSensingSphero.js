5b20d8c82348b9cb8386d3226daade12e13144e4
Xuan Shi <xuan.shi@mulesoft.com>
2014-10-03 15:53:21 -0700

    updated demo

--- example/demo/motionSensingSphero.js
+++ example/demo/motionSensingSphero.js
@@ -7,7 +7,7 @@ var app = express();
 
 // Setup Motion Hook
 var options = {
-  url: 'localhost:8080/pi/pi1/motionHook',
+  url: 'localhost:8080/pi/MuleSoftHackathonTeam1/motionHook',
   method: 'POST',
   json: {
     address: 'localhost:8000/'
@@ -19,7 +19,7 @@ request(options,
     if (!error) {
       console.log('Motion Hook setup.');
     } else {
-      console.log('Error occured %s', response.statusCode);
+      console.log('Something wrong, hook not setup');
     }
   }
 );
@@ -27,11 +27,11 @@ request(options,
 // When notified
 app.get('/', function (req, res) {
   // change sphero color
-  request('localhost:8080/sphero/sphero/color?r=255&g=0&b=0', function (error, response, body) {
+  request('localhost:8080/sphero/MuleSoftHackathonTeam1/color?r=255&g=0&b=0', function (error, response, body) {
     if (!error) {
       console.log('Color changed!');
     } else {
-      console.log('Error occured %s', response.statusCode);
+      console.log('Something wrong, hook not setup');
     }
   });
 });
